3a2ee8151b0778e3b1cd494339e810c9fdb427a2
Shubham <shubhambhutada7810@gmail.com>
2024-01-03 20:14:51 +0530

    testcase 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [fruits, setFruits] = useState(["apple", "banana", "cherry", "date", "elderberry", "fig"])
+  const [data, setData] = useState('')
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Search item</h1>
+        <input type="text" value={data} onChange={(e)=>setData(e.target.value)}/>
+        {
+          data === ""? (fruits.map((item,index) => <li key={index}>{item}</li>)) : 
+            (fruits
+              .filter(item => item.includes(data))
+              .map((item,index) => <li key={index}>{item}</li>)
+            )
+        }
+                
     </div>
   )
 }
